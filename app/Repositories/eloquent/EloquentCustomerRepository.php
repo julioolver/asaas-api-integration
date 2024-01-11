f401f5f5d72cc1bc17856f12252ff6dc8e014a63
Julio Cesar <julio.oliveira@compasso.com.br>
2024-01-10 21:08:40 -0300

    create eloquentClientRepository implements clientRepository interface

---
+++ app/Repositories/eloquent/EloquentCustomerRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories\eloquent;
+
+use App\Models\Customer;
+use App\Repositories\contracts\CustomerRepository;
+
+
+class EloquentCustomerRepository implements CustomerRepository
+{
+    public function __construct(protected Customer $model)
+    {
+    }
+
+    public function create(array $customer): Customer
+    {
+        return $this->model->create($customer);
+    }
+}
