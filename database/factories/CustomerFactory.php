3459bce09769517d64433567ecb78cb6071b1b30
Julio Cesar <julio.oliveira@compasso.com.br>
2024-01-13 00:47:22 -0300

    add field payment_gateway_id in customerFactory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -20,6 +20,7 @@ class CustomerFactory extends Factory
             "name" => fake()->name(),
             "email" => fake()->email(),
             "document_number" => fake()->numerify('###########'),
+            "payment_gateway_id" => fake()->numerify('###########'),
             "phone" => fake()->phoneNumber(),
         ];
     }
