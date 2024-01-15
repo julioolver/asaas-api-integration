7651c6b17214c0cd797fd0b3db86096c49ca32f4
Julio Cesar <julio.oliveira@compasso.com.br>
2024-01-15 17:32:17 -0300

    change fiels in DTO creditCard

--- app/DTOs/Payment/PaymentCreditCardDTO.php
+++ app/DTOs/Payment/PaymentCreditCardDTO.php
@@ -7,9 +7,9 @@ class PaymentCreditCardDTO extends BasePaymentDTO implements PaymentDTOInterface
     public CreditCardDTO $creditCard;
     public CreditCardHolderInfoDTO $holderInfo;
 
-    public function __construct($amount, $customer_id, $due_date, $method, $creditCard, $holderInfo)
+    public function __construct($customer_id, $amount, $due_date, $method, $creditCard, $holderInfo)
     {
-        parent::__construct($amount, $customer_id, $due_date, $method);
+        parent::__construct($customer_id, $amount, $due_date, $method);
         $this->creditCard = $creditCard;
         $this->holderInfo = $holderInfo;
     }
